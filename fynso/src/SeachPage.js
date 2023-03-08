1184af7f3c929de7631541e724d06b03e7eac3d0
Arunsh4h <98868947+Arunsh4h@users.noreply.github.com>
2023-03-08 21:39:31 +0530

    save

---
+++ fynso/src/SeachPage.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import "./SearchPage.scss";
+
+function SearchPage() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedOption, setSelectedOption] = useState("Video");
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    let url =
+      "https://www.google.com/search?q=" +
+      encodeURIComponent(
+        `intext:"${searchTerm
+          .replace(/[^\w\s]/gi, "")
+          .replace(/ /g, ".")}" (${getOptionValue(selectedOption)})`
+      );
+    window.open(url, "_blank");
+  };
+
+  const getOptionValue = (option) => {
+    switch (option) {
+      case "Video":
+        return "avi|mkv|mov|mp4|mpg|wmv|avchd|webm";
+      case "Audio":
+        return "ac3|flac|m4a|mp3|ogg|wav|wma|webm";
+      case "Ebook":
+        return "CBZ|CBR|CHM|DOC|DOCX|EPUB|MOBI|ODT|PDF|RTF|txt";
+      case "Pictures":
+        return "bmp|gif|jpg|png|psd|tif|tiff";
+      case "Software/Games":
+        return "apk|exe|iso|rar|tar|zip|swf";
+      case "Compressed":
+        return "7z|bz2|gz|iso|rar|zip";
+      default:
+        return "avi|mkv|mov|mp4|mpg|wmv|avchd|webm";
+    }
+  };
+
+  return (
+    <div className="App">
+      <nav>
+        <div id="nav">
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div id="od">
+        <h1>Open Directory Search Portal</h1>
+        <p>Research, Discover & Archive!</p>
+        <form onSubmit={handleFormSubmit}>
+          <div id="search-bar">
+            <div id="dropdown">
+              <select value={selectedOption} onChange={handleOptionChange}>
+                <option value="Video">Video</option>
+                <option value="Audio">Audio</option>
+                <option value="Ebook">Ebook</option>
+                <option value="Pictures">Pictures</option>
+                <option value="Software/Games">Software/Games</option>
+                <option value="Compressed">Compressed</option>
+              </select>
+            </div>
+            <input
+              id="search-box"
+              placeholder="Search here ..."
+              autoFocus
+              value={searchTerm}
+              onChange={handleInputChange}
+            />
+            <button id="search-button" type="submit">
+              <i className="fal fa-search"></i>
+            </button>
+          </div>
+        </form>
+        <nav>
+          <div id="blog">
+            <footer>
+              <p>Contact Us</p>
+            </footer>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default SearchPage;
